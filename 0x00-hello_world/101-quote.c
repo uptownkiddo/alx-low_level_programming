1a2c60ba99c6b01c2f901c4f937032a3a5392fa4
uptownkiddo <muchokilewis8@gmail.com>
2022-12-08 21:42:32 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	printf("and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 
 }
