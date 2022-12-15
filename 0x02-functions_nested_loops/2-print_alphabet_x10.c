7ac27c972d6ce34e4e42bccaa16020617dd2273e
uptownkiddo <muchokilewis8@gmail.com>
2022-12-15 15:29:13 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		_putchar('\n');
+	}
+}
