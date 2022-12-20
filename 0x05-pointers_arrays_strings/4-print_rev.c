751a447122f9fd2a59b9d63a7377ae619892dbd6
uptownkiddo <muchokilewis8@gmail.com>
2022-12-19 23:05:38 -0800

    task4,5,6,7

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that
+ * prints a string in reverse followed by a new line
+ * @s; An input character
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int string = 0;
+
+	while (s[string] != '\0')
+		string++;
+	while (string)
+		_putchar(s[--string]);
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * revstring - A function that reverses a string
+ * @s: Te integer to be reversed
+ * Return: nothing
+ */void rev_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+	char c;
+
+	while (s[a] != '\0')
+		a++;
+	while (b < a--)
+	{
+		c = s[b];
+		s[b++] = s[a];
+		s[a] = c;
+	}
+}
