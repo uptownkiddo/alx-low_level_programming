a250bb027946c463c566f2ccece05dc9ea07ab9b
uptownkiddo <muchokilewis8@gmail.com>
2023-01-18 12:17:42 -0800

    task 0

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: input string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+
+}
+
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: the pointer to the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+
+}
+
