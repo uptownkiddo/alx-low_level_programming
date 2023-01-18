0a1ddd0ff7a6f1f6418bd595cee2bd86a0a05067
uptownkiddo <muchokilewis8@gmail.com>
2023-01-18 01:28:00 -0800

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog: name of the struct
+ * description: struct with attributes of dog
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
